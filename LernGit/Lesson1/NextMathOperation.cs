e48d57f18bfd8e4cdf23378fbdf43ed6d9c627ef
Oleksiy Kovalchyk <Oleksiy Kovalchyk@DESKTOP-HF10HD4>
2018-04-23 12:22:48 +0300

    Mathod "DifferentBetweenDivAndAdd" has benn added

---
+++ LernGit/Lesson1/NextMathOperation.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson1
+{
+    public class NextMathOperation
+    {
+        public double DifferentBetweenDivAndAdd(double first, double second)
+        {
+            MathOperation mathOperation = new MathOperation();
+            return mathOperation.DivNumber(first, second) - mathOperation.AddTwoNumber(first, second);
+        }
+    }
+}
